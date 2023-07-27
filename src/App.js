86ccb295af42b99f23635a293ddd150911281195
Leo Božić <leobozic2903@gmail.com>
2023-07-27 19:08:29 +0200

    gotovo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [pojamPretrazivanja, setPojamPretrazivanja] = useState("");
+  const [podaci, setPodaci] = useState(null);
+
+  const getData = (search) => {
+    if (search !== "") {
+      fetch(`https://itunes.apple.com/search?term=${search}&entity=song`)
+        .then((response) => response.json())
+        .then((data) => {
+          setPodaci(data);
+        });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (pojamPretrazivanja) {
+      getData(pojamPretrazivanja);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={pojamPretrazivanja}
+          onChange={(e) => setPojamPretrazivanja(e.target.value)}
+        />
+        <button type="submit">Pretrazi</button>
+      </form>
+      <div></div>
+      <div>
+        {podaci && (
+          <div>
+            {podaci.results.map((podatak, index) => (
+              <li key={index}>
+                {podatak.artistName} - {podatak.trackName}
+              </li>
+            ))}
+          </div>
+        )}
+        {podaci.resultCount === 0 && <p>Nema rezultata</p>}
+      </div>
     </div>
   );
 }
